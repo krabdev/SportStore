6e29aa3a4fc3f31416b3bbc25969bf05c414dbaa
Andreas Oxinos <krabdev@icloud.com>
2013-03-23 02:03:42 +0200

    Solution Created
    - Solution created with 3 projects, domain, test, and MVC4 Basic
    template
    - Set up referenses, and some nuget packages, (jinject, mog)
    - Set up DI using Ninject

---
+++ SportStore/SportStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using SportStore.Domain.Entities;
+
+/*
+ *  This interface allow a sequence of Product objects to be obtained
+ *  A class that use the IProductRepository interface can obtain Product objects
+ *  without knowing anythink about where they are coming from or how they will be delivered!
+ */
+
+namespace SportStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; set; }
+    }
+}
